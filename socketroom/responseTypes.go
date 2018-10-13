11cb61a4e8783698993860623519f7dceb43ae3b
Will Thomas <wlthomas001@gmail.com>
2018-10-13 16:34:10 -0600

    half way through refactoring everything to accept new data structure

---
+++ socketroom/responseTypes.go
@@ -0,0 +1,33 @@
+package socketroom
+
+// RoomName contains the name in json format to send to the client.
+type RoomName struct {
+	Name string `json:"roomName"`
+}
+
+// GameMessage will be the json structure used to communicate
+type GameMessage struct {
+	Event   string      `json:"event"`
+	Payload interface{} `json:"payload"`
+}
+
+// PlayerUpdate contains a list of players and their current point.
+type PlayerUpdate struct {
+	Players []PlayerStatus `json:"players"`
+}
+
+// PlayerStatus has list of players and their current points.
+type PlayerStatus struct {
+	Name  string `json:"name"`
+	Point string `json:"point"`
+}
+
+// GAME CONSTANTS
+const (
+	joinRoom     = "JOIN_ROOM"
+	leaveRoom    = "LEAVE_ROOM"
+	submitPoint  = "SUBMIT_POINT"
+	revealPoints = "REVEAL_POINTS"
+	clearPoints  = "CLEAR_POINTS"
+	voted        = "VOTED"
+)
