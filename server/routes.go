8813835abd18f9c481d4c28143b54985927efd7e
wthomasdev <wlthomas001@gmail.com>
2019-10-29 20:55:05 -0600

    exchange temporary code for token

--- server/routes.go
+++ server/routes.go
@@ -21,14 +21,26 @@ type Server struct {
 func (s *Server) Routes() {
 	h := socketroom.NewHub()
 	sS := authentication.StateString(10)
+	endpoint := oauth2.Endpoint{
+		AuthURL:  "https://auth.atlassian.com/authorize?audience=api.atlassian.com",
+		TokenURL: "https://auth.atlassian.com/oauth/token?audience=api.atlassian.com",
+	}
+	config := oauth2.Config{
+		ClientID:     os.Getenv("JIRA_CLIENT_ID"),
+		ClientSecret: os.Getenv("JIRA_SECRET_ID"),
+		Endpoint:     endpoint,
+		RedirectURL:  "http://localhost:8080/callback",
+		Scopes:       []string{"read:jira-work"},
+	}
+	url := config.AuthCodeURL(sS)
 	go h.Run()
 	s.Router.Handle("/", http.FileServer(http.Dir("./static")))
 	s.Router.HandleFunc("/wakeup", s.wakeup())
 	s.Router.HandleFunc("/generateRoom", s.generateRoom(h))
 	s.Router.HandleFunc("/listRoomsAndClients", s.listRoomsAndClients(h))
 	s.Router.HandleFunc("/joinRoom", s.joinRoom(h))
-	s.Router.HandleFunc("/login", s.login(sS))
-	s.Router.HandleFunc("/callback", s.callback(sS))
+	s.Router.HandleFunc("/login", s.login(url))
+	s.Router.HandleFunc("/callback", s.callback(config, sS))
 }
 
 func (s *Server) wakeup() http.HandlerFunc {
@@ -67,7 +79,7 @@ func (s *Server) generateRoom(h *socketroom.Hub) http.HandlerFunc {
 	}
 }
 
-func (s *Server) callback(state string) http.HandlerFunc {
+func (s *Server) callback(config oauth2.Config, state string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		stateResp := r.FormValue("state")
@@ -76,24 +88,18 @@ func (s *Server) callback(state string) http.HandlerFunc {
 			return
 		}
 		code := r.FormValue("code")
-		resp := fmt.Sprintf("State: %s \n, Code: %s", stateResp, code)
+		token, err := config.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			fmt.Println("err", err)
+			http.Error(w, "Couldn't get token from code", 500)
+			return
+		}
+		resp := fmt.Sprintf("State: %s \n, Token: %s", stateResp, token)
 		w.Write([]byte(resp))
 	}
 }
 
-func (s *Server) login(state string) http.HandlerFunc {
-	endpoint := oauth2.Endpoint{
-		AuthURL:  "https://auth.atlassian.com/authorize?audience=api.atlassian.com",
-		TokenURL: "https://auth.atlassian.com/token?audience=api.atlassian.com",
-	}
-	config := oauth2.Config{
-		ClientID:     os.Getenv("JIRA_CLIENT_ID"),
-		ClientSecret: os.Getenv("JIRA_SECRET_ID"),
-		Endpoint:     endpoint,
-		RedirectURL:  "http://localhost:8080/callback",
-		Scopes:       []string{"read:jira-work"},
-	}
-	url := config.AuthCodeURL(state)
+func (s *Server) login(url string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, url, 301)
 	}
